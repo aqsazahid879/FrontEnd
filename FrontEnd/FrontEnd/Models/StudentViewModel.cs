6e49d92441b1adf0aca76fb57c62017b18edd9a1
aqsazahid879 <aqsazahid879@gmail.com>
2018-10-18 10:25:33 +0500

    FrontEnd

---
+++ FrontEnd/FrontEnd/Models/StudentViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FrontEnd.Models
+{
+    public class StudentViewModel
+    {
+       public string RegistrationNumber;
+       public string name;
+       public string Session;
+       public string Semester;
+    }
+}
